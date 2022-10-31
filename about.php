6d3d5e1221e3270d38da626d3aad3b67e59541dd
suarezschou <andre_suarez_schou@hotmail.com>
2022-10-31 10:54:51 +0100

    a

--- about.php
+++ about.php
@@ -12,5 +12,13 @@
     include 'nav.php';
     ?>
     <h1>About</h1>
+    <?php
+    if ($curpage == 'about.php'){
+        echo "<h2>active class</h2>";
+        echo "<img class='img-responsive' src='img/sunset.jpg'>";
+    }else{
+        echo "<p> fefs </p>";
+    }
+    ?>
 </body>
 </html>
